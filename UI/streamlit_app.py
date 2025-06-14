17a9c33a166a2c3f190187a8644ce098a8037732
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-14 23:55:57 +0300

    solve import conflicts

--- UI/streamlit_app.py
+++ UI/streamlit_app.py
@@ -1,11 +1,11 @@
 import sys
+import os
 
 __import__('pysqlite3')
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import streamlit as st
-import os
 from pathlib import Path
 from Preprocessing.document_processor import CVProcessor
 from Preprocessing.vector_store import CVVectorStore
