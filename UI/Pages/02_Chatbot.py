041f5301dfa56c50fcf7c32f6a1241188c55ce92
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-19 21:49:56 +0300

    Create 02_Chatbot.py

---
+++ UI/Pages/02_Chatbot.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.header("💬 Chatbot Q&A")
+st.info("Ask questions about the candidates.")
+st.markdown("Example: *'Who has time series experience?'*")
+st.success("Roz is listening carefully... 🎧")
+
+
