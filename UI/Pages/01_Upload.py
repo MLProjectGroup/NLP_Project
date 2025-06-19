a961fc574e0759cda11094e978649d576c778c1f
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-19 21:48:46 +0300

    Update 01_Upload.py

---
+++ UI/Pages/01_Upload.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header(" Upload CVs")
+st.info("Upload your CVs here. Supported formats: PDF, DOCX, TXT.")
+st.success("Roz is watching... 👀 Ready to analyze your CVs!")
+
