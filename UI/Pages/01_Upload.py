14106b8bb99fb491c1864f51f9ba731ce47c856f
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-20 03:26:35 +0300

    Update 01_Upload.py

--- UI/Pages/01_Upload.py
+++ UI/Pages/01_Upload.py
@@ -1,7 +1,9 @@
 # pages/01_Upload.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 import streamlit as st
-import os
 import tempfile
 from Preprocessing.document_processor import CVProcessor
 
